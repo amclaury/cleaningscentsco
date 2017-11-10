388b81bf3432f86645588e6ac8846eacc0acd6d1
allysonnn3 <33496602+allysonnn3@users.noreply.github.com>
2017-11-09 19:25:08 -0500

    Update client.php

--- client.php
+++ client.php
@@ -31,6 +31,11 @@
         <li><a href="#">PROFILE</a></li>
         <li><a href="cart.php">YOUR CART</a></li>
         <li><a href="#"><i class="material-icons">search</i></a></li>
+	<li>
+			<a id="toggle-search" href="#!">
+			<i class="large mdi-action-search"></i>
+			</a>
+        </li>
       </ul>
 
 	  <ul id="nav-mobile" class="side-nav">
