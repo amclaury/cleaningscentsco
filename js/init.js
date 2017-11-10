c03873ab3b8c42d217191a966f48cd9c2404b376
allysonnn3 <33496602+allysonnn3@users.noreply.github.com>
2017-11-09 19:35:24 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -25,6 +25,20 @@
     $('.carousel.carousel-slider').carousel('next');
   }, 5000);*/
   }); 
+ 
+  // Toggle search
+
+    $('a#toggle-search').click(function()
+    {
+        var search = $('div#search');
+
+        search.is(":visible") ? search.slideUp() : search.slideDown(function()
+        {
+            search.find('input').focus();
+        });
+
+        return false;
+    });
 
   	
 })(jQuery); // end of jQuery name space
