80ad9d903d2e35270c4f47dce4c370956bd066a3
allysonnn3 <33496602+allysonnn3@users.noreply.github.com>
2017-11-09 19:28:32 -0500

    Update catalog.php

--- catalog.php
+++ catalog.php
@@ -71,6 +71,12 @@ $current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_
         <li><a href="client.php">PROFILE</a></li>
         <li><a class="dropdown-button" data-activates="dropdown1">YOUR CART<i class="material-icons right">shopping_cart</i></a></li>
         <li><a href="#"><i class="material-icons">search</i></a></li>
+		    <li>
+            <a id="toggle-search" href="#!">
+            <i class="large mdi-action-search">
+            </i>
+			      </a>
+        </li>
       </ul>
     </div>
   </nav>
